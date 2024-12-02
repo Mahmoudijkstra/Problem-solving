898cc48b2eb40b096f5cfc6ee62b2776a550c51f
Mahmoudijkstra <188392973+Mahmoudijkstra@users.noreply.github.com>
2024-12-02 22:58:42 +0100

    Create AdventOfCode-2024/Day1Part1.cpp

---
+++ AdventOfCode-2024/Day1Part1.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+#define vi vector<int>
+#define ii pair<int,int>
+#define vii vector<ii>
+#define pb push_back
+#define ll long long
+#define fi first
+#define se second
+#define forn(i, n) for (int i = 0; i < int(n); i++)
+#define optimise ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+using namespace std;
+const int naxi=1e9+5;
+const ll naxl=1e18+5;
+const int nax=1e5+5;
+const int MOD=998244353;
+const ll INF=1e18+9;
+
+
+void test_case(){
+
+    int n=1000;
+    ll ans=0;
+    vi a(n),b(n);
+    for(int i=0;i<n;i++) {
+        cin >>a[i] >>b[i];
+    }
+    sort(a.begin(),a.end());
+    sort(b.begin(),b.end());
+    for(int i=0;i<n;i++) {
+        ans+=abs(a[i]-b[i]);
+    }
+    cout <<ans <<endl;
+
+}
+
+int main(){
+
+    optimise
+    #ifndef ONLINE_JUDGE
+        freopen("input.in", "r", stdin);
+        freopen("output.out", "w", stdout);
+    #endif
+    int t=1;
+    //cin >>t;
+    while(t--){
+        test_case();
+    }
+    return 0;
+
+}
