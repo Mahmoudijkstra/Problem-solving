5784187de29c40e23b01442b6c7078c89b839b5c
Mahmoudijkstra <188392973+Mahmoudijkstra@users.noreply.github.com>
2026-02-04 23:32:18 +0100

    Create a.cpp

---
+++ Codeforces/Div3/Codeforces Round 1076 (Div. 3)/a.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+#define vi vector<int>
+#define ii pair<int, int>
+#define vii vector<ii>
+#define pb push_back
+#define ll long long
+#define fi first
+#define se second
+#define forn(i, n) for (int i = 0; i < int(n); i++)
+#define optimise                      \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL);
+using namespace std;
+const int naxi = 1e9 + 5;
+const ll naxl = 1e18 + 5;
+const int nax = 1e5 + 5;
+const int MOD = 1e9 + 7;
+const ll INF = 1e18 + 9;
+
+void test_case()
+{
+
+    int n, s, x, sum = 0;
+    cin >> n >> s >> x;
+    for (int i = 0; i < n; i++)
+    {
+        int val;
+        cin >> val;
+        sum += val;
+    }
+    if (sum <= s)
+    {
+        string ans = (s - sum) % x ? "NO" : "YES";
+        cout << ans << endl;
+    }
+    else
+    {
+        cout << "NO" << endl;
+    }
+}
+
+int main()
+{
+
+    optimise
+#ifndef ONLINE_JUDGE
+        freopen("input.in", "r", stdin);
+    freopen("output.out", "w", stdout);
+#endif
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        test_case();
+    }
+    return 0;
+}
